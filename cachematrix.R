0a6b7796969ebb0bc6e959f246e85350f7047424
Divyanshi Mangal <divyanshidm@gmail.com>
2018-04-06 20:34:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Assignment submission by Divyanshi Mangal
 
+## makeCacheMatrix method is a list of four functions which are used to get the input matrix, get the cached result,
+## set the result and initialize the variables also.
+
+## cacheSolve method checks whether a result is already cached, if so it returns result directly,
+## else, it calculates the result store it in cache and return the result.
+
+#a function having multiple functions
 makeCacheMatrix <- function(x = matrix()) {
+          inverse <- NULL         #variable to insert cached inverse
+          
+          set <- function(y)       #function to initialize variables
+          {
+            inverse <<- NULL      
+            x <<- y
+          }
+          
+          #function to get the value of input matrix
+          get <- function() x
+          
+          #function to cache the matrix
+          setInverse <- function(newInverse) inverse<<- newInverse
+          
+          #function to get inverse from cache
+          getInverse <- function() inverse
+          list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+        }
 
-}
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        
+        #get the cached result
+        mat <- x$getInverse()
+        
+        #condtition to check whether cached result exist
+        if(!is.null(mat))
+        {
+          message("getting cached inverse matrix")
+          return(mat)
+        }
+        
+        #calculate and store the inverse if it doesn't exist
+        data<-x$get()
+        newInverse <- solve(x)
+        x$setInverse(newInverse)
+        newInverse
+  
+        }
